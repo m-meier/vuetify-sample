6c76b66388d2c1cf595b60b50df584986f2e05ab
Michael Meier <michael@meier.ws>
2021-08-31 12:19:41 +0200

    fix: move content to a row/col structure

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <v-app>
     <v-main>
-      <v-container fluid class="text-center">
-        <h1 class="mb-4">Hello World</h1>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <h1 class="mb-4">Hello World</h1>
 
-        <v-btn color="primary">
-          {{ dialog ? "Close" : "Open" }} alpha11 dialog with parent activator
+            <v-btn color="primary">
+              {{ dialog ? "Close" : "Open" }} alpha11 dialog with parent
+              activator
 
-          <v-dialog v-model="dialog" activator="parent">
-            <v-card
-              title="Dialog header"
-              text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut modi iusto ipsum molestiae doloribus rerum nulla amet eaque excepturi magni quaerat, libero porro, eligendi delectus neque sed quasi, quod sit!"
-            >
-              <template #actions>
-                <v-spacer />
-                <v-btn>Action 1</v-btn>
-                <v-btn>Action 2</v-btn>
-              </template>
-            </v-card>
-          </v-dialog>
-        </v-btn>
+              <v-dialog v-model="dialog" activator="parent">
+                <v-card
+                  title="Dialog header"
+                  text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut modi iusto ipsum molestiae doloribus rerum nulla amet eaque excepturi magni quaerat, libero porro, eligendi delectus neque sed quasi, quod sit!"
+                >
+                  <template #actions>
+                    <v-spacer />
+                    <v-btn>Action 1</v-btn>
+                    <v-btn>Action 2</v-btn>
+                  </template>
+                </v-card>
+              </v-dialog>
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
